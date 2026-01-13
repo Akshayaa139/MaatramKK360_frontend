b9984b9287a2780ed1b2388dc76572942d788f98
Akshayaa139 <akshlearn13@gmail.com>
2026-01-13 21:01:41 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: __dirname,
-  eslint: {
-    ignoreDuringBuilds: true,
+  output: "export",
+
+  images: {
+    unoptimized: true,
   },
-  env: {
-    NEXT_PUBLIC_API_URL: "http://localhost:5000",
-    NEXTAUTH_URL: "http://localhost:3000",
-  },
+
+  // 🔴 CHANGE THIS to your repo name
+  basePath: "/MaatramKK360_frontend",
+  assetPrefix: "/MaatramKK360_frontend/",
 };
 
 export default nextConfig;
+
