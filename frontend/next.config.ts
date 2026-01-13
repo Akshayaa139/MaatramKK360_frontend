02d15e7de18f1184902302e06b1161c6ef442ccc
Akshayaa139 <akshlearn13@gmail.com>
2026-01-13 21:16:26 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,15 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-
   images: {
     unoptimized: true,
   },
-
-  // 🔴 CHANGE THIS to your repo name
-  basePath: "/MaatramKK360_frontend",
-  assetPrefix: "/MaatramKK360_frontend/",
 };
 
 export default nextConfig;
+
 
